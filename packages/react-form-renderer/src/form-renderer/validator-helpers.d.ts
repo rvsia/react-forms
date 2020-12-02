dd300e4bf50f6e3dcf93f5b3f81341233d640a16
Martin Marosi <marvusm.mmi@gmail.com>
2020-12-02 08:54:54 +0100

    fix renderer tests after new build process

--- packages/react-form-renderer/src/form-renderer/validator-helpers.d.ts
+++ packages/react-form-renderer/src/form-renderer/validator-helpers.d.ts
@@ -1,8 +1,14 @@
 import { ValidatorType } from "../files/use-field-api";
 import { ReactNode } from "react";
+<<<<<<< HEAD:packages/react-form-renderer/src/form-renderer/validator-helpers.d.ts
 import { ValidatorMapper } from "../files/validator-mapper";
 import { DataType } from "../files/data-types";
 import { ValidatorFunction } from "../files/validators";
+=======
+import { ValidatorMapper } from "../validator-mapper";
+import { DataType } from "../data-types";
+import { ValidatorFunction } from "../validators/validators";
+>>>>>>> 1b5a7810 (fix renderer tests after new build process):packages/react-form-renderer/src/use-field-api/validator-helpers.d.ts
 
 export type convertToWarning = (validator: ValidatorType) => ValidatorFunction;
 
